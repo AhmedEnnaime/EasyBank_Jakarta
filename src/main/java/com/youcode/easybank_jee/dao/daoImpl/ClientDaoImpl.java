261e23bbda0a3dc5a5bd6e33d25bd45e472a73b5
AhmedEnnaime <97989224+AhmedEnnaime@users.noreply.github.com>
2023-10-16 15:13:28 +0100

    EJ-18 completed findByID method in clientDaoImpl

--- src/main/java/com/youcode/easybank_jee/dao/daoImpl/ClientDaoImpl.java
+++ src/main/java/com/youcode/easybank_jee/dao/daoImpl/ClientDaoImpl.java
@@ -5,6 +5,7 @@ import com.youcode.easybank_jee.entities.Client;
 import com.youcode.easybank_jee.utils.JPAUtil;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.TypedQuery;
 
 import java.util.List;
@@ -15,13 +16,25 @@ public class ClientDaoImpl implements ClientDao {
 
     private final EntityManager em = JPAUtil.getEntityManagerFactory().createEntityManager();
 
-    public ClientDaoImpl() {
-        em.getTransaction().begin();
-    }
+//    public ClientDaoImpl() {
+//        em.getTransaction().begin();
+//    }
 
     @Override
     public Optional<Client> create(Client client) {
-        return Optional.empty();
+        EntityTransaction transaction = em.getTransaction();
+        try {
+            transaction.begin();
+            em.persist(client);
+            transaction.commit();
+            return Optional.of(client);
+        } catch (Exception e) {
+            if (transaction.isActive()) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+            return Optional.empty();
+        }
     }
 
     @Override
@@ -31,8 +44,22 @@ public class ClientDaoImpl implements ClientDao {
 
     @Override
     public Optional<Client> findByID(Integer id) {
-        return Optional.empty();
+        EntityTransaction transaction = em.getTransaction();
+        try {
+            transaction.begin();
+            Client client = em.find(Client.class, id);
+            transaction.commit();
+
+            return Optional.ofNullable(client);
+        } catch (Exception e) {
+            if (transaction.isActive()) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+            return Optional.empty();
+        }
     }
+
 
     @Override
     public List<Client> getAll() {
