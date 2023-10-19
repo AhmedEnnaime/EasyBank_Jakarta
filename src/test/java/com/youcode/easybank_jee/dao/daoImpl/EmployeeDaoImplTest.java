ef240b8f95b5cbdebeab4f790880eddeb3fb81ad
AhmedEnnaime <97989224+AhmedEnnaime@users.noreply.github.com>
2023-10-19 19:59:20 +0100

    EJ-34 completed the testing for all methods in employeeDaoImpl

--- src/test/java/com/youcode/easybank_jee/dao/daoImpl/EmployeeDaoImplTest.java
+++ src/test/java/com/youcode/easybank_jee/dao/daoImpl/EmployeeDaoImplTest.java
@@ -4,6 +4,7 @@ import com.youcode.easybank_jee.dao.EmployeeDao;
 import com.youcode.easybank_jee.entities.Employee;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.TypedQuery;
 import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -12,6 +13,8 @@ import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 
 import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -23,6 +26,9 @@ public class EmployeeDaoImplTest {
 
     @Mock
     private EntityTransaction transaction;
+
+    @Mock
+    private TypedQuery<Employee> query;
 
     private EmployeeDao employeeDao;
 
@@ -81,5 +87,73 @@ public class EmployeeDaoImplTest {
 
         assertTrue(result.isPresent());
         assertEquals(employee, result.get());
+    }
+
+    @Test
+    @Transactional
+    void testGetAllEmployees() {
+        Employee employee1 = new Employee("Mousta", "Delegue", LocalDate.of(2001, 11, 17), "06473347924", "Jrayfat", LocalDate.of(2023, 9, 21), "mousta@gmail.com");
+        Employee employee2 = new Employee("John", "Doe", LocalDate.of(1990, 5, 15), "1234567890", "Address", LocalDate.of(2023, 9, 21), "john@example.com");
+        List<Employee> employees = Arrays.asList(employee1, employee2);
+
+        Mockito.when(entityManager.createQuery("SELECT c FROM Employee c", Employee.class)).thenReturn(query);
+
+        Mockito.when(query.getResultList()).thenReturn(employees);
+
+        List<Employee> result = employeeDao.getAll();
+
+        assertTrue(result.containsAll(employees));
+        assertEquals(employees.size(), result.size());
+    }
+
+    @Test
+    void testUpdateEmployee() {
+
+        Employee employee = new Employee(
+                "Mousta",
+                "Delegue",
+                LocalDate.of(2001, 11, 17),
+                "06473347924",
+                "Jrayfat",
+                LocalDate.of(2023, 9, 21),
+                "mousta@gmail.com"
+        );
+
+        Mockito.when(entityManager.getTransaction()).thenReturn(transaction);
+        Mockito.when(entityManager.find(Employee.class, employee.getMatricule())).thenReturn(employee);
+        Mockito.when(entityManager.merge(employee)).thenReturn(employee);
+
+        Optional<Employee> result = employeeDao.update(employee);
+
+        Mockito.verify(transaction).commit();
+
+        assertTrue(result.isPresent());
+        assertEquals(employee, result.get());
+    }
+
+    @Test
+    @Transactional
+    void testDeleteEmployee() {
+
+        Employee employee = new Employee(
+                "Mousta",
+                "Delegue",
+                LocalDate.of(2001, 11, 17),
+                "06473347924",
+                "Jrayfat",
+                LocalDate.of(2023, 9, 21),
+                "mousta@gmail.com"
+        );
+
+        int employeeId = 1;
+
+        Mockito.when(entityManager.getTransaction()).thenReturn(transaction);
+        Mockito.when(entityManager.find(Employee.class, employeeId)).thenReturn(employee);
+
+        boolean result = employeeDao.delete(employeeId);
+
+        Mockito.verify(transaction).commit();
+
+        assertTrue(result);
     }
 }
